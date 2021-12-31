4d49cdd686e3a7146b68ac0c51b96f3b9c0fcdae
Erwin Stone <erwinstone@pm.me>
2021-12-31 09:11:30 +0700

    Use native # instead of TypeScript private keyword

--- dist/input-touchspin.module.d.ts
+++ dist/input-touchspin.module.d.ts
@@ -1,15 +1,5 @@
 declare class InputTouchspin {
-    private input;
-    private btnUp;
-    private btnDown;
-    private styleId;
-    private timeout;
-    private interval;
+    #private;
     constructor(target: Element);
-    private style;
-    private events;
-    private clearTimers;
-    private step;
-    private spin;
 }
 export default InputTouchspin;
--- src/input-touchspin.ts
+++ src/input-touchspin.ts
@@ -1,77 +1,77 @@
 class InputTouchspin {
-	private input: HTMLInputElement
-	private btnUp: HTMLButtonElement
-	private btnDown: HTMLButtonElement
+	#input: HTMLInputElement
+	#btnUp: HTMLButtonElement
+	#btnDown: HTMLButtonElement
 
-	private styleId = 'input-touchspin-style'
+	#styleId = 'input-touchspin-style'
 
-	private timeout: number
-	private interval: number
+	#timeout: number
+	#interval: number
 
 	constructor(target: Element) {
-		this.input = target.querySelector('[data-touchspin-input]')
-		this.btnUp = target.querySelector('[data-touchspin-up]')
-		this.btnDown = target.querySelector('[data-touchspin-down]')
+		this.#input = target.querySelector('[data-touchspin-input]')
+		this.#btnUp = target.querySelector('[data-touchspin-up]')
+		this.#btnDown = target.querySelector('[data-touchspin-down]')
 
-		this.style()
+		this.#style()
 
-		if (!this.input.readOnly && !this.input.disabled) {
-			this.events()
+		if (!this.#input.readOnly && !this.#input.disabled) {
+			this.#events()
 		}
 	}
 
-	private style() {
-		if (document.getElementById(this.styleId) === null) {
+	#style() {
+		if (document.getElementById(this.#styleId) === null) {
 			const style = document.createElement('style')
-			style.id = this.styleId
+			style.id = this.#styleId
 			style.textContent = '[data-touchspin-input]::-webkit-inner-spin-button,[data-touchspin-input]::-webkit-outer-spin-button{margin:0;-webkit-appearance:none}[data-touchspin-input]{-moz-appearance:textfield}'
 			document.head.appendChild(style)
 		}
 	}
 
-	private events() {
+	#events() {
 		// click
-		this.btnUp.addEventListener('mousedown', () => this.spin())
-		this.btnDown.addEventListener('mousedown', () => this.spin(false))
+		this.#btnUp.addEventListener('mousedown', () => this.#spin())
+		this.#btnDown.addEventListener('mousedown', () => this.#spin(false))
 
 		// touch
-		this.btnUp.addEventListener('touchstart', (e) => {
-			this.spin()
+		this.#btnUp.addEventListener('touchstart', (e) => {
+			this.#spin()
 			e.cancelable && e.preventDefault()
 		})
-		this.btnDown.addEventListener('touchstart', (e) => {
-			this.spin(false)
+		this.#btnDown.addEventListener('touchstart', (e) => {
+			this.#spin(false)
 			e.cancelable && e.preventDefault()
 		})
 
 		// stop
 		Array.from(['mouseup', 'mouseleave', 'touchend']).forEach((e) => {
-			this.btnUp.addEventListener(e, () => this.clearTimers())
-			this.btnDown.addEventListener(e, () => this.clearTimers())
+			this.#btnUp.addEventListener(e, () => this.#clearTimers())
+			this.#btnDown.addEventListener(e, () => this.#clearTimers())
 		})
 
 		// wheel
-		this.input.addEventListener('wheel', (e) => {
-			Math.sign(e.deltaY) < 1 ? this.step() : this.step(false)
+		this.#input.addEventListener('wheel', (e) => {
+			Math.sign(e.deltaY) < 1 ? this.#step() : this.#step(false)
 			e.preventDefault()
 		})
 	}
 
-	private clearTimers() {
-		clearTimeout(this.timeout)
-		clearInterval(this.interval)
+	#clearTimers() {
+		clearTimeout(this.#timeout)
+		clearInterval(this.#interval)
 	}
 
-	private step(up = true) {
-		const before = this.input.value
-		up ? this.input.stepUp() : this.input.stepDown()
-		const after = this.input.value
-		before !== after && this.input.dispatchEvent(new Event('change'))
+	#step(up = true) {
+		const before = this.#input.value
+		up ? this.#input.stepUp() : this.#input.stepDown()
+		const after = this.#input.value
+		before !== after && this.#input.dispatchEvent(new Event('change'))
 	}
 
-	private spin(up = true) {
-		this.step(up)
-		this.timeout = setTimeout(() => (this.interval = setInterval(() => this.step(up), 50)), 300)
+	#spin(up = true) {
+		this.#step(up)
+		this.#timeout = setTimeout(() => (this.#interval = setInterval(() => this.#step(up), 50)), 300)
 	}
 }
 
